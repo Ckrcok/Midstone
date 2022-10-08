e40a08ee89b385e144b1a9b142b11b13da93f507
Maurits Dijkman <mauritsdijkman@me.com>
2022-10-07 21:25:49 -0400

    Added core components for Enemy class

---
+++ ComponentFramework/Character.h
@@ -0,0 +1,59 @@
+#ifndef CHARACTER_H
+#define CHARACTER_H
+
+#include <vector>
+
+#include "vector.h"
+
+#include "GameObject.h"
+
+
+#include "Scene.h"
+#include "KinematicBody.h"
+#include "Seek.h"
+#include "Physics.h"
+
+
+using namespace std;
+using namespace MATH;
+
+class Character
+{
+private:
+	class KinematicBody* body;
+	class Scene* scene;
+
+
+protected:
+	//bool isVisible, isActive;
+	//float moveSpeed = 4.0f;
+
+
+public:
+	Character()
+	{
+		body = NULL;
+		scene = NULL;
+	};
+	// TODO add constructor that takes parameters for the body
+	~Character()
+	{
+		if (body) delete body;
+	};
+	bool OnCreate(Scene* scene_);
+	void OnDestroy() {};
+
+	void setTexture(SDL_Texture* texture_)
+	{
+		body->setTexture(texture_);
+	}
+
+	void Update(float time);
+	void HandleEvents(const SDL_Event& event);
+	void render(float scale = 1.0f);
+
+	void steerToSeekPlayer(SteeringOutput* steering);
+
+};
+
+#endif
---
+++ ComponentFramework/GameObject.h
@@ -0,0 +1,35 @@
+#ifndef GAMEOBJECT_H
+#define GAMEOBJECT_H
+
+#include <SDL.h>
+#include <SDL_image.h>
+#include <iostream>
+#include "MMath.h"
+
+using namespace MATH;
+using namespace std;
+
+class GameObject {
+private:
+	SDL_Surface* surface;
+	SDL_Texture* texture;
+	string pathName;
+
+public:
+	GameObject(SDL_Renderer* render_, string pathName_);
+
+	void Render(Matrix4 projectionMatrix_, SDL_Renderer* renderer_,
+		float scale_, float orientationDegrees_);
+
+	// Get functions
+	SDL_Surface* GetSurface();
+	SDL_Texture* GetTexture();
+	Vec3 GetPosition();
+	string GetPathName();
+
+	// Spawnposition
+	float posX;
+	float posY;
+};
+
+#endif
