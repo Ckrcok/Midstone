951f423d893af2c6cf0a9b44cf15ce8afef39148
Maurits Dijkman <mauritsdijkman@me.com>
2022-10-17 22:30:22 -0400

    Organized GameManager & Created menu png
    Organized the GameManager to get a better understanding of the setup before adding scene selection.
    Created images folder and created an main menu lay-out.

--- ComponentFramework/SceneManager.cpp
+++ ComponentFramework/SceneManager.cpp
@@ -1,7 +1,9 @@
 #include <SDL.h>
+
 #include "SceneManager.h"
 #include "Timer.h"
 #include "Window.h"
+
 #include "Scene0.h"
 #include "Scene1.h"
 #include "Scene2.h"
@@ -9,23 +11,28 @@
 
 SceneManager::SceneManager() :
 	currentScene(nullptr), window(nullptr), timer(nullptr),
-	fps(60), isRunning(false), fullScreen(false) {
+	fps(60), isRunning(false), fullScreen(false)
+{
 	Debug::Info("Starting the SceneManager", __FILE__, __LINE__);
 }
 
-SceneManager::~SceneManager() {
-	if (currentScene) {
+SceneManager::~SceneManager()
+{
+	if (currentScene)
+	{
 		currentScene->OnDestroy();
 		delete currentScene;
 		currentScene = nullptr;
 	}
 
-	if (timer) {
+	if (timer)
+	{
 		delete timer;
 		timer = nullptr;
 	}
 
-	if (window) {
+	if (window)
+	{
 		delete window;
 		window = nullptr;
 	}
@@ -35,13 +42,15 @@ SceneManager::~SceneManager() {
 bool SceneManager::Initialize(std::string name_, int width_, int height_) {
 
 	window = new Window();
-	if (!window->OnCreate(name_, width_, height_)) {
+	if (!window->OnCreate(name_, width_, height_))
+	{
 		Debug::FatalError("Failed to initialize Window object", __FILE__, __LINE__);
 		return false;
 	}
 
 	timer = new Timer();
-	if (timer == nullptr) {
+	if (timer == nullptr)
+	{
 		Debug::FatalError("Failed to initialize Timer object", __FILE__, __LINE__);
 		return false;
 	}
@@ -53,29 +62,43 @@ bool SceneManager::Initialize(std::string name_, int width_, int height_) {
 }
 
 /// This is the whole game
-void SceneManager::Run() {
+void SceneManager::Run()
+{
 	timer->Start();
 	isRunning = true;
-	while (isRunning) {
+
+	while (isRunning)
+	{
 		timer->UpdateFrameTicks();
+
 		currentScene->Update(timer->GetDeltaTime());
 		currentScene->Render();
+
 		HandleEvents();
+
 		SDL_GL_SwapWindow(window->getWindow());
 		SDL_Delay(timer->GetSleepTime(fps));
 	}
 }
 
-void SceneManager::HandleEvents() {
+void SceneManager::HandleEvents()
+{
 	SDL_Event sdlEvent;
-	while (SDL_PollEvent(&sdlEvent)) {
-		if (sdlEvent.type == SDL_EventType::SDL_QUIT) {
+
+	while (SDL_PollEvent(&sdlEvent))
+	{
+		if (sdlEvent.type == SDL_EventType::SDL_QUIT)
+		{
 			isRunning = false;
 			return;
 		}
-		else if (sdlEvent.type == SDL_KEYDOWN) {
-			switch (sdlEvent.key.keysym.scancode) {
+		else if (sdlEvent.type == SDL_KEYDOWN)
+		{
+			switch (sdlEvent.key.keysym.scancode)
+			{
 			case SDL_SCANCODE_ESCAPE:
+				break;
+
 			case SDL_SCANCODE_Q:
 				isRunning = false;
 				return;
@@ -94,25 +117,31 @@ void SceneManager::HandleEvents() {
 				break;
 			}
 		}
-		if (currentScene == nullptr) {
+
+		if (currentScene == nullptr)
+		{
 			Debug::FatalError("Failed to initialize Scene", __FILE__, __LINE__);
 			isRunning = false;
 			return;
 		}
+
 		currentScene->HandleEvents(sdlEvent);
 	}
 }
 
-void SceneManager::BuildNewScene(SCENE_NUMBER scene) {
+void SceneManager::BuildNewScene(SCENE_NUMBER scene)
+{
 	bool status;
 
-	if (currentScene != nullptr) {
+	if (currentScene != nullptr)
+	{
 		currentScene->OnDestroy();
 		delete currentScene;
 		currentScene = nullptr;
 	}
 
-	switch (scene) {
+	switch (scene)
+	{
 	case SCENE_NUMBER::SCENE0:
 		currentScene = new Scene0();
 		status = currentScene->OnCreate();
@@ -139,5 +168,3 @@ void SceneManager::BuildNewScene(SCENE_NUMBER scene) {
 		break;
 	}
 }
-
-
